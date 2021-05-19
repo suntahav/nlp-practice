a9510a1eb22b17149c614405502e89c91068a65f
Sandeep <vijeta013@gmail.com>
2021-05-20 01:41:39 +0530

    Task: Word Counter Done

---
+++ build_pipeline/count_words.py
@@ -0,0 +1,34 @@
+import re
+def count_words(text):
+    """Count how many times each unique word occurs in text."""
+    counts = dict()  # dictionary of { <word>: <count> } pairs to return
+    
+    # TODO: Convert to lowercase
+    text = text.lower()
+    expression = re.compile(r'[^a-zA-Z0-9]') # could have used r'[^\w]
+    # TODO: Split text into tokens (words), leaving out punctuation
+    # (Hint: Use regex to split on non-alphanumeric characters)
+    word_list = list(filter(None, re.split(expression,text)))
+    # TODO: Aggregate word counts using a dictionary
+    for value in set(word_list):
+        counts[value] = word_list.count(value)
+    return counts
+
+
+def test_run():
+    with open("build_pipeline/data/input.txt", "r") as f:
+        text = f.read()
+        counts = count_words(text)
+        sorted_counts = sorted(counts.items(), key=lambda pair: pair[1], reverse=True)
+        
+        print("10 most common words:\nWord\tCount")
+        for word, count in sorted_counts[:10]:
+            print("{}\t{}".format(word, count))
+        
+        print("\n10 least common words:\nWord\tCount")
+        for word, count in sorted_counts[-10:]:
+            print("{}\t{}".format(word, count))
+
+
+if __name__ == "__main__":
+    test_run()
